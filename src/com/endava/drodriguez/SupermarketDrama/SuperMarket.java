1094004774051685bca098fd37ec324d2ef3fd03
Juan David <david.rodriguez@endava.com>
2018-08-14 14:29:56 -0500

    refactored and documented both algorithms

--- src/com/endava/drodriguez/SupermarketDrama/SuperMarket.java
+++ src/com/endava/drodriguez/SupermarketDrama/SuperMarket.java
@@ -11,6 +11,11 @@ class SuperMarket {
         this.capacity = capacity;
     }
 
+    /**
+     * main method to solve the problem. Joins all the other functions in logical order to solve the SuperMarket
+     * situation
+     * @return array of 0's and 1's representing de elements that should be taken as solution to the given problem.
+     */
     int[] result() {
 
         int sum = -1;
@@ -24,9 +29,16 @@ class SuperMarket {
             }else if(tempSum == this.capacity) return items.clone();
         }
 
+
+
         return positions;
     }
 
+    /**
+     * given an array of 0's and 1's, sums the elements of the shopping list wich correspond with the 1's in the input
+     * @param positions array of 0's and 1's representing which elements of the shopping list to sum
+     * @return total sum of the value of the elements selected from the parameter
+     */
     private int getSumFromPositions(int[] positions){
         int sum = 0;
 
@@ -36,6 +48,13 @@ class SuperMarket {
         return sum;
     }
 
+    /**
+     * Creates a matrix where each row represents a combination of elements to try. The matrix is used to
+     * make all the combinations of n elements of the list. That is, it tries all combinations of one element,
+     * two elements, etc.
+     * @param listSize number of items in the shopping list
+     * @return matrix with @param{listSize} columns and 2^@param{listSize} rows.
+     */
     private static int[][] binaryNumberGenerator(int listSize){
         int numberPositions = (int)Math.pow(2, listSize-1);
         int[][] matrix = new int[numberPositions][listSize];
--- src/com/endava/drodriguez/WhereAmI/Maze.java
+++ src/com/endava/drodriguez/WhereAmI/Maze.java
@@ -1,5 +1,9 @@
 package com.endava.drodriguez.WhereAmI;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import static com.endava.drodriguez.WhereAmI.Maze.Direction.*;
 
 
@@ -13,6 +17,13 @@ class Maze {
         this.finalPoint = finalPoint;
     }
 
+    /**
+     * initial setting for the maze solution finder. Finds the path that will solve the maze problem
+     * and re-formats its values, assigning 1's to the path positions of the solution, and 0's otherwise
+     * (walls and unvisited positions).
+     *
+     * @return matrix with the solution for the given maze
+     */
     int[][] getPath() {
         int[][] path = getPath(this.maze, this.initPoint);
 
@@ -25,40 +36,69 @@ class Maze {
         return path;
     }
 
-    private int[][] getPath(int[][] history, int[] actualPosition) {
+    /**
+     * Recursive method to find the path from an actual point to the objective point
+     *
+     * @param history        matrix that contains the maze (walls) and the previously visited cells. It is used to
+     *                       determine where is it reasonable to move, i.e. cells that are not walls and that have
+     *                       not been visited before
+     * @param actualPosition array with [x, y] coordinates representing the actual position in the maze
+     * @return matrix containing the maze and the path taken to solve the maze problem. The matrix contains
+     * 1's for walls, 0's for unvisited positions, and -1's representing the solution path of the maze
+     */
+    @Nullable
+    private int[][] getPath(@NotNull int[][] history, int[] actualPosition) {
+
+        if (actualPosition == null || !coordinateIsInMaze(actualPosition) || history[actualPosition[0]][actualPosition[1]] != 0)
+            return null;
+
+
         history[actualPosition[0]][actualPosition[1]] = -1;
         if (actualPosition[0] == this.finalPoint[0] && actualPosition[1] == this.finalPoint[1]) return history;
 
+        //the four cardinal coordinates moving one step from the actual position
         int[] northPos = moveStep(actualPosition, NORTH),
                 southPos = moveStep(actualPosition, SOUTH),
                 eastPos = moveStep(actualPosition, EAST),
                 westPos = moveStep(actualPosition, WEST);
 
-        if (northPos != null && coordinateIsInMaze(northPos) && history[northPos[0]][northPos[1]] == 0) {
-            int[][] path = getPath(history, northPos);
-            if (path != null) return path; else history[northPos[0]][northPos[1]]=0;
-        }
-        if (southPos != null && coordinateIsInMaze(southPos) && history[southPos[0]][southPos[1]] == 0) {
-            int[][] path = getPath(history, southPos);
-            if (path != null) return path; else history[southPos[0]][southPos[1]]=0;
-        }
-        if (eastPos != null && coordinateIsInMaze(eastPos) && history[eastPos[0]][eastPos[1]] == 0) {
-            int[][] path = getPath(history, eastPos);
-            if (path != null) return path; else history[eastPos[0]][eastPos[1]]=0;
-        }
-        if (westPos != null && coordinateIsInMaze(westPos) && history[westPos[0]][westPos[1]] == 0) {
-            int[][] path = getPath(history, westPos);
-            if (path != null) return path; else history[westPos[0]][westPos[1]]=0;
-        }
-        return null;
+        //can I get a solution moving to the given direction?
+        int[][] path = getPath(history, northPos);
+        path = path == null ? getPath(history, southPos) : path;
+        path = path == null ? getPath(history, eastPos) : path;
+        path = path == null ? getPath(history, westPos) : path;
+
+        //after moving in all four cardinal points, if no solution path is obtained,
+        // remove position from history
+        if(path==null) history[actualPosition[0]][actualPosition[1]] = 0;
+
+        return path;
     }
 
-    private boolean coordinateIsInMaze(int[] coordinate) {
-        return coordinate[0] > -1 && coordinate[0] < maze.length &&
-                coordinate[1] > -1 && coordinate[1] < maze[0].length;
+    /**
+     * determine if a given [x, y] point falls within the range of the maze
+     *
+     * @param coordinate array of [x, y] coordinates of the point to evaluate
+     * @return true if the point falls within the range of the maze, false otherwise
+     */
+    @Contract(pure = true)
+    private boolean coordinateIsInMaze(@NotNull int[] coordinate) {
+        return coordinate[0] > -1
+                && coordinate[0] < maze.length
+                && coordinate[1] > -1
+                && coordinate[1] < maze[0].length;
     }
 
-    private int[] moveStep(int[] actualPosition, Direction direction) {
+    /**
+     * from the actual position, return the new position, when moving in a determined direction
+     *
+     * @param actualPosition array with [x, y] coordinates before moving
+     * @param direction      cardinal direction to move from the actual position
+     * @return array with [x, y] coordinates of position after moving in the given direction
+     */
+    @NotNull
+    @Contract(pure = true)
+    private int[] moveStep(int[] actualPosition, @NotNull Direction direction) {
         switch (direction) {
             case NORTH:
                 return new int[]{actualPosition[0] - 1, actualPosition[1]};
